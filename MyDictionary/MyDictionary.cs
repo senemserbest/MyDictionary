d69f9d000b6e384be41759e1456cfad508dd4fd0
Senem SERBEST <senemserbest@eskisehir.edu.tr>
2021-02-11 01:48:40 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Y, İ>
+    {
+
+        Y[] Yemek;
+        İ[] İl;
+
+        public MyDictionary()
+        {
+            Yemek = new Y[0];
+            İl = new İ[0];
+
+        }
+
+        public void Add(Y key, İ item)
+        {
+            Y[] tempKeyArray = Yemek;
+            İ[] tempValueArray = İl;
+
+            Yemek = new Y[Yemek.Length + 1];
+            İl = new İ[İl.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                Yemek[i] = tempKeyArray[i];
+                İl[i] = tempValueArray[i];
+
+            }
+
+            Yemek[Yemek.Length - 1] = key;
+            İl[İl.Length - 1] = item;
+
+        }
+        public void List()
+        {
+            for (int i = 0; i < Yemek.Length; i++)
+            {
+                Console.WriteLine("İl: " + İl[i] + " Yemek: " + Yemek[i]);
+
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> yemekler = new MyDictionary<string, string>();
+            yemekler.Add("Çibörek", "Eskişehir");
+            yemekler.Add("Tantuni", "Mersin");
+            yemekler.Add("Mantı", "Kayseri");
+
+            yemekler.List();
+
+        }
+    }
+}
